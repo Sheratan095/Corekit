d98b896f54c80e472933de264a66a876fc9e7d3f
Marco <marco.ceccarelli.code@gmail.com>
2024-08-23 17:58:50 +0200

    ifx is_string_empty

--- src/string/ft_is_string_empty.c
+++ src/string/ft_is_string_empty.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_is_string_empty.c                               :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: maceccar <maceccar@student.42firenze.it>   +#+  +:+       +#+        */
+/*   By: marvin <marvin@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/12 16:36:08 by maceccar          #+#    #+#             */
-/*   Updated: 2024/08/12 17:08:32 by maceccar         ###   ########.fr       */
+/*   Updated: 2024/08/23 17:56:30 by marvin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,10 +17,13 @@ t_bool	ft_is_string_empty(char	*string)
 	int	i;
 
 	i = 0;
+	if (!string)
+		return (true);
 	while (string[i])
 	{
 		if (!ft_isspace(string[i]))
 			return (false);
+		i++;
 	}
 	return (true);
 }
