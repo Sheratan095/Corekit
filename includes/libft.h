26f0f67dd9d5ee3c3079e4f7eabddfb84c82391a
Marco Ceccarelli <marco.ceccarelli.code@gmail.com>
2025-02-24 16:05:47 +0100

    add return value to ft_putendl_fd

--- includes/libft.h
+++ includes/libft.h
@@ -132,7 +132,7 @@ int		ft_putchar_fd(char c, int fd);
 
 int		ft_putstr_fd(char *s, int fd);
 
-void	ft_putendl_fd(char *s, int fd);
+int		ft_putendl_fd(char *s, int fd);
 
 int		ft_putnbr_fd(int n, int fd);
 
--- src/output/ft_putendl_fd.c
+++ src/output/ft_putendl_fd.c
@@ -12,10 +12,14 @@
 
 #include "libft.h"
 
-void	ft_putendl_fd(char *s, int fd)
+int	ft_putendl_fd(char *s, int fd)
 {
+	int	result;
+
+	result = 0;
 	if (!s || fd < 0)
 		return ;
-	ft_putstr_fd(s, fd);
-	ft_putchar_fd('\n', fd);
+	result += ft_putstr_fd(s, fd);
+	result += ft_putchar_fd('\n', fd);
+	return (result);
 }
